1d1d8d1e69083c026bef61b480c4310de5bd1626
David <destrada2193@gmail.com>
2021-10-18 18:42:09 -0700

    Complete queue binary numbers algo

---
+++ src/Queue/GenerateBinaryNumbers.java
@@ -0,0 +1,72 @@
+package Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class GenerateBinaryNumbers {
+	
+	// Generate the First N Binary Numbers
+	// Input: int n
+	// Output: void (side effect, print out the first N binary numbers)
+	
+	
+	public static void main(String[] args) {
+		printBinary(1);
+		printBinary(3);
+		printBinary(10);
+		printBinary(0); // nothing to print
+		printBinary(-9); // nothing to print 
+		
+		printBinaryUsingQueue(1);
+		printBinaryUsingQueue(3);
+		printBinaryUsingQueue(10);
+		printBinaryUsingQueue(0); // nothing to print
+		printBinaryUsingQueue(-9); // nothing to print
+		
+		
+		
+	}
+	
+	public static void printBinary(int n) {
+		if (n <= 0) {
+			System.out.println("Nothing to print\n");
+			return;
+		}
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.println(Integer.toBinaryString(i)); // this built-in function converts base-10 decimals to binary
+		}
+		
+		System.out.println();
+	}
+	
+	// We can use a queue to leverage the binary pattern of starting with 1, then appending 0s,
+	// then replacing the 0 with a 1 (shifting the 1s to the higher position), then appending 0s and starting over...
+	// 1
+	// 10
+	// 11
+	// 100
+	// 101 
+	// 110 
+	public static void printBinaryUsingQueue(int n) {
+		if (n <= 0) {
+			System.out.println("Nothing to print\n");
+			return;
+		}
+		
+		Queue<Integer> queue = new LinkedList<>();
+		
+		queue.add(1);
+		
+		for (int i = 1; i <= n; i++) {
+			Integer current = queue.remove();
+			System.out.println(current);
+			
+			queue.add(current * 10);
+			queue.add(current * 10 + 1);
+		}
+		
+		System.out.println();
+	}
+
+}
